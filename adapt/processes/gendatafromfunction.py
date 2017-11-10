d38ef52f8e6ff03cdfc1da40f650cf42d3e54797
Christoph Rosemann <christoph.rosemann@desy.de>
2017-11-10 20:40:52 +0100

    to calculate data points from fit values, e.g. for subtraction
    still some issue left: the evaluation can't be overridden from the initial points used for/in the fit
    or I still haven't found what I'm looking for -- the proper way to do it

---
+++ adapt/processes/gendatafromfunction.py
@@ -0,0 +1,56 @@
+# Copyright (C) 2017  Christoph Rosemann, DESY, Notkestr. 85, D-22607 Hamburg
+# email contact: christoph.rosemann@desy.de
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation in  version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA  02110-1301, USA.
+
+# generate points according to fitted model for the given fit results
+
+from adapt import iProcess
+
+
+class gendatafromfunctiondef(iProcess.IProcessDefinition):
+
+    def __init__(self):
+        super(gendatafromfunctiondef, self).__init__()
+        self._ptype = "gendatafromfunction"
+        self.createParameter("fitresult", "STRING")
+        self.createParameter("xdata", "STRING")
+        self.createParameter("output", "STRING")
+
+class gendatafromfunction(iProcess.IProcess):
+
+    def __init__(self, procDef):
+        super(gendatafromfunction, self).__init__(procDef)
+        self._fitresult = self._parameters["fitresult"]
+        self._xdata = self._parameters["xdata"]
+        self._output = self._parameters["output"]
+
+    def initialize(self, data):
+        pass
+
+    def execute(self, data):
+        fitresult = data.getData(self._fitresult)
+        xdata = data.getData(self._xdata)
+
+        # TODO: this doesn't evaluate at all data points!
+        # TODO: it still uses only the data points from fit time
+        data.addData(self._output, fitresult.eval(data=xdata))
+        
+    def finalize(self, data):
+        pass
+
+    def check(self, data):
+        pass
