a5428407b42d5db12c8fe648f3e8750ce4704584
Christoph Rosemann <christoph.rosemann@desy.de>
2017-12-13 15:57:39 +0100

    completed the basic cfg handler

--- adapt/configurationHandler.py
+++ adapt/configurationHandler.py
@@ -19,13 +19,40 @@
 # this is the abstract definition of the persistency of configuration
 # currently only one method is foreseen, but maybe this wil change
 
+import yaml
+
 import iConfigurationHandler
+import processingConfiguration
 
 
 class ConfigurationHandler(iConfigurationHandler.IConfigurationHandler):
 
-    def loadConfig(self, filename):
-        pass
+    def __init__(self):
+        self._datablob = None
+        self._procConfig = processingConfiguration.ProcessingConfiguration()
 
-    def writeConfig(self, filename, config):
-        pass
+    def loadConfig(self, filename):
+        with open(filename) as blob:
+            self._datablob = yaml.load(blob)
+        self._serialize()
+        return self._procConfig
+
+    def writeConfig(self, filename, procconf):
+        dumpDict = {}
+        dumpDict["execlist"] = procconf.getOrderOfExecution()
+        for k,v in procconf.getProcessDefinitions().items:
+            dumpDict[k] = v
+        
+        with open(filename, 'w') as outfile:
+            yaml.dump(dumpDict, outfile, default_flow_style=False)
+
+    def _serialize(self):
+        try:
+            self._procConfig.setOrderOfExecution(self._datablob["execlist"])
+        except KeyError("No [execlist] item in config. Exiting."):
+            exit()
+        try:
+            for procname in self._execList:
+                self._procConfig.addSingleProcessDefinition(procname, self._datablob[procname])
+        except KeyError("Item in execlist has no corresponding entry."):
+            exit()
