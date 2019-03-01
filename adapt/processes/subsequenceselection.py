003add3cf93e5fcd132e9b28db2562e3677d8ad8
Christoph Rosemann <christoph.rosemann@desy.de>
2019-03-01 15:06:12 +0100

    + fix issue #105 with subsequence selection; needs more care

--- adapt/processes/subsequenceselection.py
+++ adapt/processes/subsequenceselection.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017  Christoph Rosemann, DESY, Notkestr. 85, D-22607 Hamburg
+# Copyright (C) 2017-9  Christoph Rosemann, DESY, Notkestr. 85, D-22607 Hamburg
 # email contact: christoph.rosemann@desy.de
 #
 # This program is free software; you can redistribute it and/or
@@ -30,18 +30,23 @@ class subsequenceselection(IProcess):
         self._inputsPar = ProcessParameter("input", list)
         self._outputPar = ProcessParameter("output", list)
         self._selectionmethodsPar = ProcessParameter("selectors", list)
-        self._startpointsPar = ProcessParameter("startpointnumber", int, optional=True)
-        self._endpointsPar = ProcessParameter("endpointnumber", int, optional=True)
-        # self._startendindicesPar = ProcessParameter("startendindices", list, optional=True)
+        self._startpointsPar = ProcessParameter("startpointnumber", int, None, optional=True)
+        self._endpointsPar = ProcessParameter("endpointnumber", int, None, optional=True)
+        self._slicestartPar = ProcessParameter("slicestart", int, None, optional=True)
+        self._sliceendPar = ProcessParameter("sliceend", int, None, optional=True)
+        self._slicestridePar = ProcessParameter("slicestride", int, None, optional=True)
         self._parameters.add(self._inputsPar)
         self._parameters.add(self._outputPar)
         self._parameters.add(self._selectionmethodsPar)
         self._parameters.add(self._startpointsPar)
         self._parameters.add(self._endpointsPar)
-        # self._parameters.add(self._startendindicesPar)
+        self._parameters.add(self._slicestartPar)
+        self._parameters.add(self._sliceendPar)
+        self._parameters.add(self._slicestridePar)
+
         self._methodDict = {"selectfromstart": self._selectFromStart,
                             "selectfromend": self._selectFromEnd,
-                            # "selectSection" : self._selectSection,
+                            "selectslice" : self._slice,
                             }
 
     def initialize(self):
@@ -50,7 +55,9 @@ class subsequenceselection(IProcess):
         self._selectionmethods = self._selectionmethodsPar.get()
         self._startpoints = self._startpointsPar.get()
         self._endpoints = self._endpointsPar.get()
-        # self._startendindices = self._startendindicesPar.get()
+        self._slicestart = self._slicestartPar.get()
+        self._sliceend = self._sliceendPar.get()
+        self._slicestride = self._slicestridePar.get()
 
         # check if the methods are available and parameter logic is correct
         for method in self._selectionmethods:
@@ -62,26 +69,33 @@ class subsequenceselection(IProcess):
             raise ValueError("[subsequenceselection] Method selectfromstart needs a startpointnumber.")
         if "selectfromend" in self._selectionmethods and not self._endpoints:
             raise ValueError("[subsequenceselection] Method selectfromend needs a endpointnumber.")
-        # if "selectSection" in self._selectionmethods and not self._startendindices:
-            # raise ValueError("[subsequenceselection] Method selectSection needs startendindices.")
+        if "selectslice" in self._selectionmethods and self._slicestart is None or self._sliceend is None:
+            raise ValueError("[subsequenceselection] Method selectslice needs start and end values.")
+        if "selectslice" in self._selectionmethods and len(self._selectionmethods) > 1:
+            raise ValueError("[subsequenceselection] Method selectslice cannot be combined with another selection method.")
 
     def execute(self, data):
-        # outer loop: over the inputs
-        for index in range(len(self._inputs)):
-            sequence = data.getData(self._inputs[index])
-            tempout = []
-            if "selectfromstart" in self._selectionmethods:
-                tmp = self._selectFromStart(sequence, self._startpoints)
-                tempout.append(self._selectFromStart(sequence, self._startpoints))
-            if "selectfromend" in self._selectionmethods:
-                tmp = self._selectFromEnd(sequence, self._endpoints)
-                tempout.append(self._selectFromEnd(sequence, self._endpoints))
-            # if "selectSection" in self._selectionmethods:
-                # tempout.append(self._selectSection(sequence, self._startendindices))
-            out = tempout[0]
-            for elem in tempout[1:]:
-                out = np.concatenate((out, elem))
-            data.addData(self._output[index], out)
+        # start or endselection:
+        if "selectfromstart" in self._selectionmethods or "selectfromend" in self._selectionmethods:
+            for index in range(len(self._inputs)):
+                sequence = data.getData(self._inputs[index])
+                tempout = []
+                if "selectfromstart" in self._selectionmethods:
+                    tmp = self._selectFromStart(sequence, self._startpoints)
+                    tempout.append(self._selectFromStart(sequence, self._startpoints))
+                if "selectfromend" in self._selectionmethods:
+                    tmp = self._selectFromEnd(sequence, self._endpoints)
+                    tempout.append(self._selectFromEnd(sequence, self._endpoints))
+                # if "selectSection" in self._selectionmethods:
+                    # tempout.append(self._selectSection(sequence, self._startendindices))
+                out = tempout[0]
+                for elem in tempout[1:]:
+                    out = np.concatenate((out, elem))
+                data.addData(self._output[index], out)
+        elif "selectslice" in self._selectionmethods:
+            for index in range(len(self._inputs)):
+                sequence = data.getData(self._inputs[index])
+ 
 
     def finalize(self, data):
         pass
@@ -99,3 +113,6 @@ class subsequenceselection(IProcess):
     def _selectFromEnd(self, seq, num):
         '''create a subsequence that only has the last num elements of seq'''
         return seq[(-1)*num:]
+
+    def _slice(self, seq):
+        pass
